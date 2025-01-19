adfab6f7a874161603889bbb2aa3ae591e01aa63
Ankush Kawde <mr.kawde@gmail.com>
2025-01-19 17:47:07 +0530

    added guess number game code

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,101 @@
+import { StyleSheet, TextInput, View, Alert, Text } from "react-native";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import { useState } from "react";
+import Colors from "../constants/colors";
+import Title from "../components/ui/Title";
+import Card from "../components/ui/Card";
+import InstructionText from "../components/ui/InstructionText";
+
+const StartGameScreen = ({ onPickNumber }) => {
+  const [enteredNumner, setEnteredNumner] = useState("");
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumner(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnteredNumner("");
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumner);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid number",
+        "Number has to be a number between 1 and 99",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+      );
+      return;
+    }
+
+    onPickNumber(chosenNumber);
+  }
+
+  return (
+    <View style={styles.rootContainer}>
+      <Title>Guess My Number</Title>
+      <Card>
+        <InstructionText>Enter a number</InstructionText>
+        <TextInput
+          style={styles.numberInput}
+          maxLength={2}
+          keyboardType="number-pad"
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={enteredNumner}
+          onChangeText={numberInputHandler}
+        />
+        <View style={styles.btnsContainer}>
+          <View style={styles.btnContainer}>
+            <PrimaryButton onPress={resetInputHandler}>Restart</PrimaryButton>
+          </View>
+          <View style={styles.btnContainer}>
+            <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+          </View>
+        </View>
+      </Card>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    marginTop: 100,
+    alignItems: "center",
+  },
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 36,
+    marginHorizontal: 24,
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+    elevation: 4, // android specific
+    shadowColor: "black", // IOS specific
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 60,
+    width: 60,
+    fontSize: 32,
+    color: Colors.accent500,
+    borderBottomColor: Colors.accent500,
+    borderBottomWidth: 2,
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  btnsContainer: {
+    flexDirection: "row",
+  },
+  btnContainer: {
+    flex: 1,
+  },
+});
